c14964736588c6d0ff882d5ef86c8d021a542d2a
Samuel Osa-Agbontaen <sosaagbontaen@college.harvard.edu>
2023-02-15 01:54:08 -0500

    Added concept labels

--- app.py
+++ app.py
@@ -6,20 +6,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+#SOFTWARE CONCEPT : CLASSES
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-print("✅APP RUNNING!✅")
-
+#SOFTWARE CONCEPT : ROUTING
 @app.route('/')
 def index():
+    #SOFTWARE CONCEPT : SERVERLESS DATABASE
     #show each task by querying database
     task_list = Task.query.all()
-    print(task_list)
+    #SOFTWARE CONCEPTS : REUSABLE COMPONENTS, PROPERTIES
     return render_template("base.html", task_list_param=task_list)
 
+#SOFTWARE CONCEPT : ROUTING
 @app.route("/add", methods=["POST"])
 def add():
     # add new task
@@ -29,6 +31,7 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
+#SOFTWARE CONCEPT : ROUTING
 @app.route("/update/<int:task_id>")
 def update(task_id):
     # update task
@@ -37,6 +40,7 @@ def update(task_id):
     db.session.commit()
     return redirect(url_for("index"))
 
+#SOFTWARE CONCEPT : ROUTING
 @app.route("/delete/<int:task_id>")
 def delete(task_id):
     # delete task
@@ -49,9 +53,5 @@ def delete(task_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
-        #new_task = Task(title="Task 1", complete=False)
-        #db.session.add(new_task)
-        #db.session.commit()
 
     app.run(debug=True)
